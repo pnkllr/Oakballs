3a19553f9156badfc95f161bbf321f8eb9fc9457
PnKllr <pnkllr@gmail.com>
2022-06-15 18:55:04 +1000

    Removed timers for later use

--- index.js
+++ index.js
@@ -177,14 +177,14 @@ function onMysterysubHandler(channel, username, numbOfSubs, userstate) {
 }
 
 // ➤ T I M E R S
-function StreamTimer() {
-    Twitch.say(process.env.CHANNEL_NAME, 'enjoying stream? Then why dont you leave a follow, say something in chat or even go follow PnKllr on social media');
-}
-setInterval(StreamTimer, 1.2e+6);
+// function StreamTimer() {
+//     Twitch.say(process.env.CHANNEL_NAME, 'enjoying stream? Then why dont you leave a follow, say something in chat or even go follow PnKllr on social media');
+// }
+// setInterval(StreamTimer, 1.2e+6);
 // 1.5e+6 = timer goes off every 20 mins
-
-function DiscTimer() {
-    Twitch.say(process.env.CHANNEL_NAME, 'enjoying talking here? Continue the conversation over on Discord! https://discord.gg/qrFtuzn7jQ');
-}
-setInterval(DiscTimer, 1.8e+6);
+// 
+// function DiscTimer() {
+//     Twitch.say(process.env.CHANNEL_NAME, 'enjoying talking here? Continue the conversation over on Discord! https://discord.gg/qrFtuzn7jQ');
+// }
+// setInterval(DiscTimer, 1.8e+6);
 // 1.8e+6 = timer goes off every 30 mins
