c9bcd2d0d56318f46d58fc249251db8f0ffed3dd
PnKllr <pnkllr@gmail.com>
2022-06-16 22:46:29 +1000

    cleanup

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const fs = require('fs');
 const data = JSON.parse(fs.readFileSync("values.json"));
 require('dotenv').config()
 
-// ➤ S T A R T    B L O C K E D    W O R D S
+// ➤ S T A R T    B L O C K E D    W O R D S //
 const BLOCKED_WORDS = [
     'f4f',
     'follow me',
 ]
 
-// ➤ S T A R T    O F    B O T   C O D E
+const colors = ["SpringGreen", "Blue", "Chocolate", "Red", "Coral", "Firebrick", "OrangeRed", "SeaGreen", "Green", "HotPink"];
+
+// ➤ S T A R T    O F    B O T   C O D E //
 const Twitch = new tmi.Client({
     options: { debug: true, messagesLogLevel: "info" },
     connection: {
@@ -48,26 +50,52 @@ Discord.login(process.env.DISCORD_BOT_TOKEN).then(() => {
     Discord.user.setActivity(`http://twitch.tv/pnkllr`, { type: 'WATCHING' });
 });
 
-const subchannel = Discord.channels.cache.get('976101213986779166');
+const trackChannel = Discord.channels.cache.get('976101213986779166');
 
-// ➤ C H A N N E L   E V E N T S
+// ➤ D I S C O R D   E V E N T S
+// Member Joins
+Discord.on('guildMemberAdd', member => {
+    trackChannel.send(`\`\`\`diff\n+ ${member}\`\`\``);
+});
+
+// Member Leaves
+Discord.on('guildMemberRemove', member => {
+    trackChannel.send(`\`\`\`diff\n- ${member}\`\`\``);
+});
+
+// ➤ T W I T C H   C H A N N E L   E V E N T S
+// Hosted
 Twitch.on('hosted', (channel, username, viewers, autohost) => {
-    onHostedHandler(channel, username, viewers, autohost)
+    Twitch.say(channel, `Really @${username}? You want to share this with ${viewers} other people? Really?`);
 });
+
+// Raided
 Twitch.on('raided', (channel, username, viewers) => {
-    onRaidedHandler(channel, username, viewers)
+    Twitch.say(channel, `Oh hey @${username} and their ${viewers} minions o/`);
 });
+
+// Sub
 Twitch.on('subscription', (channel, username, message, userstate) => {
-    onSubscriptionHandler(channel, username, message, userstate)
+    trackChannel.send(`\`\`\`asciidoc\n= New Subscriber =\n[${username}]\`\`\``);
+    Twitch.say(channel, `Oh no! @${username} is wasting money =O`);
 });
+
+// Resub
 Twitch.on('resub', (channel, username, message, userstate) => {
-    onResubHandler(channel, username, message, userstate)
+    trackChannel.send(`\`\`\`asciidoc\n= x${userstate["msg-param-cumulative-months"]} Month Subscriber =\n[${username}] :: ${message}\`\`\``);
+    Twitch.say(channel, `I guess you didn't learn the first time hey @${username}?`);
 });
+
+// Gift Sub
 Twitch.on("subgift", (channel, username, recipient, userstate) => {
-    onGiftsubHandler(channel, username, recipient, userstate)
+    trackChannel.send(`\`\`\`asciidoc\n= ${username} Gifted a Sub  =\n[${recipient}]\n\nThey have gifted a total of ${userstate["msg-param-sender-count"]} subs\`\`\``);
+    Twitch.say(channel, `Im sure they have their own money @${username}`);
 });
+
+// Multible Gift Sub
 Twitch.on("submysterygift", (channel, username, numbOfSubs, userstate) => {
-    onMysterysubHandler(channel, username, numbOfSubs, userstate)
+    trackChannel.send(`\`\`\`asciidoc\n= ${username} Gifted ${numbOfSubs} Subs =\nThey have gifted a total of ${userstate["msg-param-sender-count"]} subs\`\`\``);
+    Twitch.say(channel, `While im sure they have their own money, its no doubt you are now broke @${username}`);
 });
 
 // CHECK IF MESSAGE WAS SENT BY VIEWER
@@ -92,13 +120,16 @@ Twitch.on('message', (channel, userstate, message, self) => {
     // START COMMANDS
     switch (message) {
         case '!discord':
-            Twitch.say(channel, `@${userstate.username}, This is the server you're looking for https://discord.gg/qrFtuzn7jQ`);
+            Twitch.say(channel, `@${userstate.username}, This is the server you're looking for https://discord.gg/UyQR5m6ACR`);
             break;
         case '!website':
             Twitch.say(channel, `@${userstate.username}, Don't forget to add it to your bookmarks! https://pnkllr.net`);
             break;
         case '!socials':
             Twitch.say(channel, `Twitter: PnKllr || IG: PnKllrTV || YouTube: PnKllr`);
+            break;
+        case '!gt':
+            Twitch.say(channel, `PnKllr || PnKllrTV`);
             break;
         case '!cc':
             Twitch.say(channel, `Use my Epic Creator Code when you make purchases in the Epic store and Fortnite: PnKllr`);
@@ -144,41 +175,13 @@ function checkTwitchChat(userstate, message, channel) {
     }
 }
 
-// ON HOST
-function onHostedHandler(channel, username, viewers) {
-    Twitch.say(channel, `Really @${username}? You want to share this with ${viewers} other people? Really?`);
-}
-
-// ON RAID
-function onRaidedHandler(channel, username, viewers) {
-    Twitch.say(channel, `Oh hey @${username} and their ${viewers} minions o/`);
-}
-
-// ON SUB
-function onSubscriptionHandler(channel, username) {
-    subchannel.send(`\`\`\`asciidoc\n= New Subscriber =\n[${username}]\`\`\``);
-    Twitch.say(channel, `Oh no! @${username} is wasting money =O`);
-}
-
-// ON RESUB
-function onResubHandler(channel, username, userstate, message) {
-    subchannel.send(`\`\`\`asciidoc\n= x${userstate["msg-param-cumulative-months"]} Month Subscriber =\n[${username}] :: ${message}\`\`\``);
-    Twitch.say(channel, `I guess you didn't learn the first time hey @${username}?`);
-}
-
-// ON GIFT SUB
-function onGiftsubHandler(channel, username, recipient, userstate) {
-    subchannel.send(`\`\`\`asciidoc\n= ${username} Gifted a Sub  =\n[${recipient}]\n\nThey have gifted a total of ${userstate["msg-param-sender-count"]} subs\`\`\``);
-    Twitch.say(channel, `Im sure they have their own money @${username}`);
-}
-
-// ON MYSTERY GIFT SUB
-function onMysterysubHandler(channel, username, numbOfSubs, userstate) {
-    subchannel.send(`\`\`\`asciidoc\n= ${username} Gifted ${numbOfSubs} Subs =\nThey have gifted a total of ${userstate["msg-param-sender-count"]} subs\`\`\``);
-    Twitch.say(channel, `While im sure they have their own money, its no doubt you are now broke @${username}`);
-}
-
 // ➤ T I M E R S
+function colorChange() {
+    Twitch.color(colors[Math.floor(Math.random() * 10)]);
+}
+setInterval(colorChange, 300000);
+//300000 = timer goes off every 5 mins
+
 // function StreamTimer() {
 //     Twitch.say(process.env.CHANNEL_NAME, 'enjoying stream? Then why dont you leave a follow, say something in chat or even go follow PnKllr on social media');
 // }
